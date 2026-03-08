52fff57927117e3d9a7390471a43c100c81619d9
Carterman555 <25chart555@gmail.com>
2026-03-07 21:42:39 -0700

    Basic typing

--- src/main.cpp
+++ src/main.cpp
@@ -3,24 +3,44 @@
 
 using namespace std;
 
-int main()
-{
-	sf::RenderWindow window( sf::VideoMode( { 200, 200 } ), "SFML works!" );
-	sf::CircleShape shape( 100.f );
-	shape.setFillColor( sf::Color::Green );
 
-	while ( window.isOpen() )
-	{
-		while ( const std::optional event = window.pollEvent() )
-		{
-			if ( event->is<sf::Event::Closed>() )
+int main() {
+
+
+	sf::RenderWindow window(sf::VideoMode({ 800, 800 }), "My Window");
+	window.setPosition({50, 50});
+
+	sf::Font font("CONSOLA.TTF");
+	sf::Text text(font);
+
+	text.setString("Hello");
+	text.setCharacterSize(24);
+	text.setFillColor(sf::Color::White);
+
+
+	while (window.isOpen()) {
+		while (const std::optional event = window.pollEvent()) {
+			if (event->is<sf::Event::Closed>()) {
 				window.close();
+			}
+			else if (const auto* keyPressed = event->getIf<sf::Event::KeyPressed>()) {
+				if (keyPressed->scancode == sf::Keyboard::Scancode::Escape)
+					window.close();
+			}
+			else if (const auto* textEntered = event->getIf<sf::Event::TextEntered>()) {
+				if (textEntered->unicode < 128) {
+					char c = static_cast<char>(textEntered->unicode);
+					cout << c << endl;
+					text.setString(text.getString() + c);
+				}
+			}
 		}
 
-		window.clear();
-		window.draw( shape );
+		window.clear(sf::Color(25, 25, 25));
+
+		window.draw(text);
+
 		window.display();
 	}
 
-	cout << "test" << endl;
 }
