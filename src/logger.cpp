6077b32786a017e3f4c834d5d0276eeed8eeffd4
Carterman555 <25chart555@gmail.com>
2026-08-11 09:24:23 -0600

    add std:: to logger.cpp

--- src/logger.cpp
+++ src/logger.cpp
@@ -1,22 +1,22 @@
 #include "logger.hpp"
 
 sf::Clock Logger::clock;
-queue<Log> Logger::log_queue;
-unordered_set<int> Logger::ids;
+std::queue<Log> Logger::log_queue;
+std::unordered_set<int> Logger::ids;
 
-void Logger::log_delayed(int id, string_view message) {
+void Logger::log_delayed(int id, std::string_view message) {
 
     bool already_queued = ids.count(id);
     if (!already_queued) {
         ids.insert(id);
 
-        Log log(id, string(message));
+        Log log(id, std::string(message));
         log_queue.push(log);
     }
 
     if (clock.getElapsedTime().asMilliseconds() >= 1000) {
         while (!log_queue.empty()) {
-            cout << log_queue.front().message << endl;
+            std::cout << log_queue.front().message << std::endl;
             log_queue.pop();
         }
         ids.clear();
